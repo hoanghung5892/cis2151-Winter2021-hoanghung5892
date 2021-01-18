c7b22249f4b32e6ce776ec2f7a71464894c0b7bf
hoanghung5892 <77470221+hoanghung5892@users.noreply.github.com>
2021-01-18 12:52:11 -0500

    project_0_CIS_2151

---
+++ Project_0/src/Project0ThursdayNight.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class Project0ThursdayNight {
+        public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Hello generic humaroid, I am the mighty computer, what is your name?");
+        String name = keyboard.nextLine();
+        System.out.println("Hello "+name+", nice to meet you, how are you?");
+        keyboard.nextLine();
+        System.out.println("That's nice! I am alway FIRE!!!");
+       
+    }
+    
+}
